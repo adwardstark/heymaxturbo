528d62ae893e15d3f0e80cd4b1f224879969f77b
Aditya Awasthi <aditya.awasthi@aspireapp.com>
2025-02-01 13:57:58 +0530

    Added base expo-app with nativewind configured

---
+++ apps/mobile/tailwind.config.js
@@ -0,0 +1,17 @@
+import nativewind from "nativewind/preset";
+import sharedConfig from "@package/ui/tailwind.config.ts";
+
+export default {
+  content: ["*.{js,jsx,ts,tsx}", "./src/**/*.{ts,tsx}"],
+  presets: [sharedConfig, nativewind],
+  theme: {
+    extend: {
+      fontFamily: {
+        interRegular: ["Inter_400Regular"],
+        interMedium: ["Inter_500Medium"],
+        interSemiBold: ["Inter_600SemiBold"],
+        interBold: ["Inter_700Bold"],
+      },
+    },
+  },
+};
